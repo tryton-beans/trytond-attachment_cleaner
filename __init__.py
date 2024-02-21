63379b75c0c2dd8c8fd06e72b6ad1ecb5b7f7b58
francesc <francesc.guillen@gmail.com>
2024-02-21 12:43:46 +0100

    Add missing import hy

--- __init__.py
+++ __init__.py
@@ -1,6 +1,7 @@
 # This file is part of the multiple_attachment module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
+import hy
 from trytond.pool import Pool
 from . import attachment
 
